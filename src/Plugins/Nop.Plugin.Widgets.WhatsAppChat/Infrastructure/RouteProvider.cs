2d75678d3903b93beb957a64c0395d0faba4d7cb
Adrian Dodan <36422231+adriandodan@users.noreply.github.com>
2024-11-06 16:30:02 +0200

    Feature/adding whats app chat (#2)
    * Created Whatsapp widget
    
    * Added TestMessage to whatsapp - not working yet
    
    * Finished whatsapp plugin
    
    ---------
    
    Co-authored-by: Adrian Dodan <adrian.dodan@luminoslabs.com>

---
+++ src/Plugins/Nop.Plugin.Widgets.WhatsAppChat/Infrastructure/RouteProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.Widgets.WhatsAppChat.Infrastructure;
+
+public class RouteProvider : IRouteProvider
+{
+    public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapControllerRoute(name: WhatsAppChatDefaults.ConfigurationRouteName,
+            pattern: "Admin/WhatsAppChat/Configure",
+            defaults: new { controller = "WhatsAppChat", action = "Configure" });
+    }
+
+    public int Priority => 0;
+}
